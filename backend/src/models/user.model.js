e48e07238fba9652679b9b314325208cbe4ac48c
Bhavya-Dhanawani <bhavyadhanwani1234@gmail.com>
2026-06-19 21:08:00 +0530

    Added the user modules

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,1 +1,28 @@
-import mongoose
+// Importing modules
+import mongoose from "mongoose";
+
+// Defining the user schema
+const userSchema = new mongoose.Schema(
+    {
+        name : {
+            type : String,
+            required : true
+        },
+        email : {
+            type : String,
+            required : true,
+            unique : true
+        },
+        password : {
+            type : String,
+            required : true
+        },
+    },
+    {
+        timestamps : true
+    }
+);
+
+// Creating the user model
+const User = mongoose.model("User", userSchema);
+export default User;
